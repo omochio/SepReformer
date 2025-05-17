1f69bb96a1bd8cabebc26d0fc32eda285e9d9e79
omochio <omochiodev@gmail.com>
2025-05-17 07:46:01 +0000

    Remove wandb attribute from Libri2Mix model engine to fix training error and fix config

--- models/SepReformer_Base_Libri2Mix/engine.py
+++ models/SepReformer_Base_Libri2Mix/engine.py
@@ -8,7 +8,7 @@ from loguru import logger
 from tqdm import tqdm
 from utils import util_engine, functions
 from utils.decorators import *
-from torch.utils.tensorboard import SummaryWriter
+from torch.utils.tensorboard.writer import SummaryWriter
 
 
 @logger_wraps()
@@ -210,11 +210,10 @@ class Engine(object):
                         test_loss_src_time_1, test_loss_src_time_2, test_num_batch = self._test(self.dataloaders['test'])
                         on_test_end = time.time()
                         logger.info(f"[TEST] Loss(time/mini-batch) \n - Epoch {epoch:2d}: SISNRi = {test_loss_src_time_1:.4f} dB | SDRi = {test_loss_src_time_2:.4f} dB | Speed = ({on_test_end - on_test_start:.2f}s/{test_num_batch:d})")
-                    valid_loss_best = util_engine.save_checkpoint_per_best(valid_loss_best, valid_loss_src_time, train_loss_src_time, epoch, self.model, self.main_optimizer, self.checkpoint_path, self.wandb_run)
-                    # Logging to monitoring tools (Tensorboard && Wandb)
+                    valid_loss_best = util_engine.save_checkpoint_per_best(valid_loss_best, valid_loss_src_time, train_loss_src_time, epoch, self.model, self.main_optimizer, self.checkpoint_path)
                     writer_src.add_scalars("Metrics", {
                         'Loss_train_time': train_loss_src_time, 
                         'Loss_valid_time': valid_loss_src_time}, epoch)
-                    writer_src.add_scalars("Learning Rate", self.main_optimizer.param_groups[0]['lr'], epoch)
+                    writer_src.add_scalar("Learning Rate", self.main_optimizer.param_groups[0]['lr'], epoch)
                     writer_src.flush()
                 logger.info(f"Training for {self.config['engine']['max_epoch']} epoches done!")
